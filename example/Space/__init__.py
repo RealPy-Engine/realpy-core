a29599fe2d24a5aec05748ed15622fce9ec87547
iconstudio <yoyofa2@hotmail.com>
2021-03-08 16:21:15 +0900

    Example is modified.

--- example/Space/__init__.py
+++ example/Space/__init__.py
@@ -6,23 +6,10 @@ from RsCore.sprite import RsSprite
 from RsCore import constants as RsConstants, containers as RsContainers
 from RsCore.assets import room_register, instance_create
 
-import Game.game_cfg as GameConstants
-
-
-
 def init():
-    room_register("roomInit")
-    room_register("roomLogo")
-    room_register("roomIntro")
-    room_register("roomMain")
-    room_register("roomDemo")
-    room_register("roomStage01")
-    room_register("roomStage02")
-    room_register("roomStage03")
-
     # Test
-    from Game.spaceships import oSpaceShip
-    TestInstance = instance_create(oSpaceShip, GameConstants.LAYERS.PLAYER_7, 0, 50)
+    from .spaceships import oSpaceShip
+    TestInstance = instance_create(oSpaceShip, "Instances", 0, 50)
     print(TestInstance)
 
 
--- example/test_main.py
+++
@@ -1,7 +0,0 @@
-from ..src import RsCore
-
-
-if __name__ == "__main__":
-    RsCore.init("RealPy Engine", 640, 480)
-    RsCore.room_register("roomTest")
-    RsCore.startup()
