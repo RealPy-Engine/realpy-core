25463db977d0acf404468b9a8a234b1a20c30a6d
iconstudio <yoyofa2@hotmail.com>
2021-03-11 09:16:39 +0900

    The utility is integrated in one file

--- realpy/utility/__init__.py
+++ realpy/utility/__init__.py
@@ -1,12 +1,19 @@
 import math
 import random
+from typing import Union
+
+__all__ = [
+    "sqr", "sign", "degtorad", "radtodeg", "irandom", "irandom_range", "bezier4", "choose",
+    "distribute", "probability_test", "lengthdir_x", "lengthdir_y",
+    "point_distance", "point_direction"
+]
 
 
-def sqr(v):
+def sqr(v: float) -> float:
     return v * v
 
 
-def sign(x):
+def sign(x: float) -> int:
     ret = 0
     if x > 0:
         ret = 1
@@ -15,22 +22,22 @@ def sign(x):
     return ret
 
 
-def degtorad(degree):
+def degtorad(degree: float) -> float:
     return math.radians(degree * math.pi / 180)
 
 
-def radtodeg(radian):
+def radtodeg(radian: float) -> float:
     return math.degrees(radian)
 
 
-def bezier4(t, x1, x2, x3, x4):
+def bezier4(t, x1, x2, x3, x4) -> float:
     factor = 1 - t
 
     return factor * (factor * (factor * x1 + t * x2)
                      + t * (factor * x2 + t * x3)) + t * (factor * (factor * x2 + t * x3) + t * (factor * x3 + t * x4))
 
 
-def lengthdir_x(length, direction):
+def lengthdir_x(length: float, direction: float) -> float:
     return math.cos(degtorad(direction)) * length
 
 
@@ -38,30 +45,30 @@ def lengthdir_y(length, direction):
     return -math.sin(degtorad(direction)) * length
 
 
-def point_distance(X1, Y1, X2, Y2):
+def point_distance(X1: float, Y1: float, X2: float, Y2: float) -> float:
     return math.dist([X1, Y1], [X2, Y2])
 
 
-def point_direction(X1, Y1, X2, Y2):
+def point_direction(X1: float, Y1: float, X2: float, Y2: float) -> float:
     return radtodeg(math.atan2(Y2 - Y1, X1 - X2))
 
 
-def irandom(n):
+def irandom(n: Union[int, float]) -> int:
     return random.randint(0, int(n))
 
 
-def irandom_range(n1, n2):
+def irandom_range(n1: Union[int, float], n2: Union[int, float]) -> int:
     return random.randint(int(n1), int(n2))
 
 
-def distribute(x1, x2, ratio):
+def distribute(x1: float, x2: float, ratio: float) -> float:
     if irandom(100) <= ratio * 100:
         return x1
     else:
         return x2
 
 
-def probability_test(max):
+def probability_test(max: Union[int, float]) -> bool:
     return bool(irandom(max - 1) == 0)
 
 
