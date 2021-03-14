380c0842267b6c4c763a2ea799648ff2ace12d24
iconstudio <yoyofa2@hotmail.com>
2021-03-15 00:23:25 +0900

    The prefab is restructured.

--- realpy/prefab/__init__.py
+++ realpy/prefab/__init__.py
@@ -1,4 +1,9 @@
-from .primitive import RsPrefab
+""" Prefab
+    ---
+    `from realpy import RsPrefab`
+    `from realpy import RsInstance`
+"""
+from .header import RsPrefab
 from .instance import RsInstance
 
-__all__ = ("RsPrefab", "RsInstance")
+__all__ = ["RsPrefab", "RsInstance"]
