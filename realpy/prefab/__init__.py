2ec4c1c276c44912f75e6efed34649681fa07b2b
iconstudio <yoyofa2@hotmail.com>
2021-03-15 02:34:38 +0900

    Todos are added.

--- realpy/prefab/__init__.py
+++ realpy/prefab/__init__.py
@@ -9,3 +9,8 @@ from .header import RsPrefab
 from .instance import RsInstance
 
 __all__ = ["RsPrefab", "RsInstance"]
+
+
+# TODO: #24 Make a test from prefab.
+def test():
+    print("Test → Realpy Prefab")
