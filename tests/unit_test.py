8ff6f59f6ac2a9d8de4bb30f24a80698560bac1a
iconstudio <yoyofa2@hotmail.com>
2021-03-15 01:32:06 +0900

    Total test unit is added.

---
+++ tests/unit_test.py
@@ -0,0 +1,11 @@
+import realpy
+
+realpy.scene.test()
+realpy.layer.test()
+realpy.preset.test()
+realpy.utility.test()
+realpy.image.test()
+realpy.sprite.test()
+# realpy.prefab.test()
+# realpy.asset.test()
+
