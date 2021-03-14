b5a4c16bb820fe1bfa821b9716cbb86d0ef31374
iconstudio <yoyofa2@hotmail.com>
2021-03-14 21:54:54 +0900

    The layer is restructured. Test available.

--- realpy/layer/__init__.py
+++ realpy/layer/__init__.py
@@ -1,37 +1,73 @@
-class RsLayer(object):
-    """ RsLayer(name)
+""" Layer
+    ---
+    `from realpy import RsLayer`
+"""
+from .header import RsLayer
 
-        Belongs to a scene and contains game instances.
-    """
+__all__ = ["RsLayer"]
 
-    def __init__(self, name: str=""):
-        from realpy.prefab import RsInstance
 
-        self.name: str = name
-        self.storage: list[RsInstance] = []
+def test():
+    print("Test → Realpy Layer")
 
-    def __str__(self) -> str:
-        return self.name
+    class PseudoInstance1:
+        def __init__(self) -> None:
+            self.x = 0
 
-    def __repr__(self) -> str:
-        return f"Realpy Layer {self.name} ({len(self.storage)})"
+        def onAwake(self):
+            print("Instance 1: ", id(self))
 
-    def onAwake(self) -> None:
-        for Instance in self.storage:
-            Instance.onAwake()
+        def onDestroy(self):
+            print("Instance 1 died! - ", id(self))
 
-    def onDestroy(self) -> None:
-        for Instance in self.storage:
-            Instance.onDestroy()
+        def onUpdate(self, time: float):
+            self.x += 5 * time
 
-    def onUpdate(self, time: float) -> None:
-        for Instance in self.storage:
-            Instance.onUpdate(time)
 
-    def onUpdateLater(self, time: float) -> None:
-        for Instance in self.storage:
-            Instance.onUpdateLater(time)
+    class PseudoInstance2:
+        def onAwake(self):
+            print("Instance 2: ", id(self))
 
-    def onDraw(self, time: float) -> None:
-        for Instance in self.storage:
-            Instance.onDraw(time)
+        def onDestroy(self):
+            print("Instance 2 died! - ", id(self))
+
+        def onUpdate(self, time: float):
+            print("Instance 2 -> middle")
+
+        def onUpdateLater(self, time: float):
+            print("Instance 2 -> later")
+
+
+    try:
+        print("Class of scene: ", RsLayer)
+        print("Class of simulated instance #1: ", PseudoInstance1)
+        print("Class of simulated instance #2: ", PseudoInstance2)
+
+        Sample = RsLayer("Sample Layer")
+        print("Sample layer: ", Sample)
+
+        Instance1 = Sample.add_instance(PseudoInstance2())
+        Instance2 = Sample.add_instance(PseudoInstance1())
+        Instance3 = Sample.add_instance(PseudoInstance2())
+        print("Sample Instance 1: ", Instance1)
+        print("Sample Instance 2: ", Instance2)
+        print("Sample Instance 3: ", Instance3)
+        print("The instances: ", Sample.storage)
+
+        Sample.onAwake()
+        for i in range(10):
+            Sample.onUpdate(1)
+        Sample.onDestroy()
+
+        print("Resulf - Sample Instance 2: ", Instance2.x)
+    except TypeError as e:
+        print("Type Error: ", e)
+        return False
+    except AttributeError as e:
+        print("Attribute Error: ", e)
+        return False
+    except Exception as e:
+        print("Error: ", e)
+        return False
+    return True
+
