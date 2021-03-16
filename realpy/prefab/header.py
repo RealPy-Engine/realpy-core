dc08b06ad59cb4cf0fc4ae9a46d009123ec8925a
iconstudio <yoyofa2@hotmail.com>
2021-03-17 02:59:13 +0900

    Made some plans

--- realpy/prefab/header.py
+++ realpy/prefab/header.py
@@ -3,14 +3,18 @@ from typing import Type, Optional, List
 from ..sprite import RsSprite
 
 
-class RsPrefab(object):
+# TODO: #33 Adopt better way to instancing prefabs
+class RsPrefab(type):
     """`RsPrefab`
         ---
         A template class of game object. Contains its own sprite and hierachies.
 
         Do not instantiate this primitive prefab.
     """
+    from .instance import RsInstance
+
     sprite_index: Optional[RsSprite] = None
+    implement = RsInstance
 
     def __new__(cls, *args, **kargs):
         cls.__parent: Optional[Type[RsPrefab]] = None
@@ -90,7 +94,6 @@ class RsPrefab(object):
             ---
             Creates a instance of game object.
         """
-        from .instance import RsInstance
 
-        Result = RsInstance(cls, scene, layer, x, y)
+        Result = cls.implement(cls, scene, layer, x, y)
         return Result
