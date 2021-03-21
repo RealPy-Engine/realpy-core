cd0556efb0304434d2882096d7ebdd16fe5f548f
iconstudio <yoyofa2@hotmail.com>
2021-03-21 22:49:08 +0900

    THe prefab is simplified.

--- realpy/prefab/header.py
+++ realpy/prefab/header.py
@@ -10,32 +10,11 @@ class RsPrefab(object):
     """
 
     sprite_index = None
-    __parent = None
-    children: List[Type] = []
     use_collision: bool = True
 
     def __repr__(self) -> str:
         SpriteCheck = f" ({self.sprite_index})" if self.sprite_index else ""
         return f"Prefab" + SpriteCheck + f" of {type(self)}"
-
-    @property
-    @classmethod
-    def parent(cls):
-        return cls.__parent
-
-    @parent.setter
-    @classmethod
-    def parent(cls, target: Optional[Type]):
-        if not target:
-            if cls.__parent:
-                cls.__parent.children.remove(cls)
-            cls.__parent = None
-        else:
-            Parent = cls.__parent
-            if Parent:
-                Parent.children.remove(cls)
-            cls.__parent = target
-            target.children.append(cls)
 
     @staticmethod
     def onAwake(itself) -> None:
