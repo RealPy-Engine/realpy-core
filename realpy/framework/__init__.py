d40983e6b7225d4bc3b108761bc262adc85a053c
iconstudio <yoyofa2@hotmail.com>
2021-03-24 16:30:40 +0900

    The test cases for layer and preset are removed.

--- realpy/framework/__init__.py
+++ realpy/framework/__init__.py
@@ -21,10 +21,44 @@ def test():
 
         async def timeout():
             await asyncio.sleep(5)
+            print("x of - Sample Instance 2:", TestInstance2.x)
             raise TimeoutError
 
-        class oTest(RsPrefab):
-            pass
+
+        class oSampleObject1(RsPrefab):
+            @staticmethod
+            def onAwake(itself):
+                print("Instance 1: ", id(itself))
+
+            @staticmethod
+            def onDestroy(itself):
+                print("Instance 1 died! - ", id(itself))
+
+            @staticmethod
+            def onUpdate(itself, time: float):
+                itself.x += 5 * time
+
+
+        class oSampleObject2(RsPrefab):
+            @staticmethod
+            def onAwake(itself):
+                print("Instance 2: ", id(itself))
+
+            @staticmethod
+            def onDestroy(itself):
+                print("Instance 2 died! - ", id(itself))
+
+            @staticmethod
+            def onUpdate(itself, time: float):
+                print("Instance 2 -> middle")
+
+            @staticmethod
+            def onUpdateLater(itself, time: float):
+                print("Instance 2 -> later")
+
+
+        print(">>> Class of prefab #1: ", oSampleObject1)
+        print(">>> Class of prefab #2: ", oSampleObject2)
 
         RsFramework.rs_init("RealPy Engine", 200, 200)
         asyncio.run(timeout())
@@ -35,9 +69,15 @@ def test():
         Testbed = TestRoom.add_layer_direct(RsLayer("Instances"))
         print(repr(Testbed))
 
-        TestInstance1 = instance_create(oTest, Testbed)
-        print(TestInstance1)
-        print(repr(TestInstance1))
+        TestInstance1 = instance_create(oSampleObject1, Testbed)
+        TestInstance2 = instance_create(oSampleObject2, Testbed)
+        TestInstance3 = instance_create(oSampleObject2, Testbed)
+        print("Sample Instance 1:", TestInstance1)
+        print("Sample Instance 2:", TestInstance2)
+        print("Sample Instance 3:", TestInstance3)
+        print("The instances: ", Testbed.storage)
+
+        RsFramework.rs_startup()
     except AttributeError as e:
         print("Attribute Error: ", e)
         return False
--- realpy/preset/__init__.py
+++ realpy/preset/__init__.py
@@ -6,15 +6,3 @@
 """
 from .header import *
 from . import header as RsPreset
-
-
-# TODO: #22 Make a test from preset.
-def test():
-    print("***** Test → Realpy Settings *****")
-
-    try:
-        print(">>> Module of setting: ", RsPreset)
-    except Exception as e:
-        print("Error: ", e)
-        return False
-    return True
