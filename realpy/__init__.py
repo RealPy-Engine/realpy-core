d22561047dd09d24c1350e62f5598e8565ed2854
iconstudio <yoyofa2@hotmail.com>
2021-03-26 01:10:52 +0900

    Massive key constants are added.

--- realpy/__init__.py
+++ realpy/__init__.py
@@ -4,10 +4,11 @@ from .layer import *
 from .image import *
 from .sprite import *
 from .behavior import *
-
+from .collisionfield import *
+from .constants import *
 from .preset import *
 from .arithmetic import *
 from .asset import *
-from .__constants import *
+from .__function import *
 
-__version__ = "1.0.0"
+__version__ = "0.9.9"
