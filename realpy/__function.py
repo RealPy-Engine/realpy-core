fe561bbb6262e6f81144673a31c041361ee89716
iconstudio <yoyofa2@hotmail.com>
2021-03-26 00:21:56 +0900

    Input methods are added.

--- realpy/__function.py
+++ realpy/__function.py
@@ -1,14 +1,51 @@
-from .preset import *
-from .arithmetic import *
-from .asset import *
-from .__constants import *
+from realpy.constants import INPUT_STATES
+from realpy.preset import RsPreset
 
 
-# TODO: keyboard_check, mouse_check
+# TODO: #40 input wrapper functions
 def keyboard_check(key: int) -> bool:
-    ...
+    Place = RsPreset.KeyEvents.get(key)
+    if Place and Place == INPUT_STATES.ING or Place == INPUT_STATES.PRESSED:
+        return True
+    else:
+        return False
+
+
+def keyboard_check_pressed(key: int) -> bool:
+    Place = RsPreset.KeyEvents.get(key)
+    if Place and Place == INPUT_STATES.PRESSED:
+        return True
+    else:
+        return False
+
+
+def keyboard_check_released(key: int) -> bool:
+    Place = RsPreset.KeyEvents.get(key)
+    if Place and Place == INPUT_STATES.RELEASED:
+        return True
+    else:
+        return False
 
 
 def mouse_check(button: int) -> bool:
-    ...
+    Place = RsPreset.MouseEvents.get(button)
+    if Place and Place == INPUT_STATES.ING or Place == INPUT_STATES.PRESSED:
+        return True
+    else:
+        return False
 
+
+def mouse_check_pressed(button: int) -> bool:
+    Place = RsPreset.MouseEvents.get(button)
+    if Place and Place == INPUT_STATES.PRESSED:
+        return True
+    else:
+        return False
+
+
+def mouse_check_released(button: int) -> bool:
+    Place = RsPreset.MouseEvents.get(button)
+    if Place and Place == INPUT_STATES.RELEASED:
+        return True
+    else:
+        return False
