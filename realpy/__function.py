82114beb386716cb81a142d5556ff4b04e858785
Iconer <yoyofa2@hotmail.com>
2021-03-25 16:35:39 +0900

    Create __function.py

---
+++ realpy/__function.py
@@ -0,0 +1,14 @@
+from .preset import *
+from .arithmetic import *
+from .asset import *
+from .__constants import *
+
+
+# TODO: keyboard_check, mouse_check
+def keyboard_check(key: int) -> bool:
+    ...
+
+
+def mouse_check(button: int) -> bool:
+    ...
+
