3d5e08d3130f1e529c800a17f836c829deb42fb0
iconstudio <yoyofa2@hotmail.com>
2021-04-05 11:55:35 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="realpy-engine-iconer",  # Replace with your own username
-    version="1.0.0a",
+    version="1.0.0",
     author="Iconerworks",
     author_email="yoyofa2@hotmail.com",
     description="2D game engine with pygame",
